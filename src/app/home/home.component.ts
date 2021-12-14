4df49e2fe753112c72246de98f135c3d3730a8cb
stanmozolevskiy <stanmozolevskiy@gmail.com>
2021-12-14 09:47:47 -0500

    `deploy`

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +12,19 @@ export class HomeComponent implements OnInit {
 faBriefcase = faBriefcase;
 faEnvelope = faEnvelope;
 
-  constructor() { }
+  constructor(private meta: Meta,private titleService: Title,) { }
 
   ngOnInit(): void {
+    this.meta.addTag({
+      name: 'Stan Mozolevskiy',
+      content: 'Software Engineer'
+    });
+    this.meta.updateTag(
+      {
+        name: 'description',
+        content: 'Cette application a été développée avec Angular version 11.2.10 et bootstrap 4.6.0' +
+          ' Elle applique le Routing, le Lazy loading, le Server side rendering et les Progressive Web App (PWA)'
+      });
   }
 
 }
