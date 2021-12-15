c9e035dc7972aad1fa69836ad4b325ebf8741561
stanmozolevskiy <stanmozolevskiy@gmail.com>
2021-12-15 08:57:21 -0500

    `deploy`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireFunctionsModule } from '@angular/fire/functions';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -11,6 +13,16 @@ import { AboutComponent } from './about/about.component';
 import { GetintouchComponent } from './getintouch/getintouch.component';
 
 
+const config = {
+  apiKey: "AIzaSyDYfIolSWky_mSTDqu5ukJ5pApqksK_Ri0",
+  authDomain: "email-handler-e3a22.firebaseapp.com",
+  projectId: "email-handler-e3a22",
+  storageBucket: "email-handler-e3a22.appspot.com",
+  messagingSenderId: "540805341175",
+  appId: "1:540805341175:web:d929602239ff84a10858f2",
+  measurementId: "G-MP9LNCRW5F"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,14 +30,18 @@ import { GetintouchComponent } from './getintouch/getintouch.component';
     ContactComponent,
     AboutComponent,
     GetintouchComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    ReactiveFormsModule 
+    ReactiveFormsModule ,
+    AngularFireModule.initializeApp(config),
+    AngularFireFunctionsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireFunctions } from '@angular/fire/functions';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -8,17 +9,29 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
   contactForm!:FormGroup;
-  constructor(private fB: FormBuilder,) { }
+  constructor(private fB: FormBuilder, private fun: AngularFireFunctions) { }
 
   ngOnInit(): void {
    this.contactForm = this.fB.group({
       name:'',
       email:'',
-      text:'',
+      message:'',
     })
   }
   onSubmit(){
-    console.log(this.contactForm.value);
+  //  console.log(this.contactForm.value);
     this.contactForm.reset();
+    this.sendEmail();
+  }
+
+  sendEmail() {
+    const callable = this.fun.httpsCallable('emailMessage');
+    
+    callable({ 
+      message: 'This is my first sendgrid email via Firebase functions',
+      name: 'Abdul Muhamed', 
+      email:"mozolevski90@gmail.com"})
+      .subscribe(
+      res=> console.log(res));
   }
 }
