4994c4227f45707c471cb9a7799e618d74f34a96
stanmozolevskiy <stanmozolevskiy@gmail.com>
2021-12-21 15:10:08 -0500

    `deploy`

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   age!:number;
+  iconSize:number = 25;
   constructor() { }
 
   ngOnInit(): void {
